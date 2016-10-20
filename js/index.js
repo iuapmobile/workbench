576be65a6dc1032ff87dfd90d9f2893c8ebd9e34
gct <gct@yonyou.com>
2016-10-20 11:02:33 +0800

    tt

---
+++ js/index.js
@@ -0,0 +1,160 @@
+﻿summerready = function() {
+	// here is your code...
+	var y = $summer.offset($summer.byId('header')).h;
+	//var width = $summer.offset(document.getElementsByTagName("body")[0]).w;
+	//var height = $summer.offset($summer.byId('main')).h;
+
+	summer.openFrame({
+		name : 'main',
+		url : 'html/main.html',
+		bounces : true,
+		rect : {
+			x : 0,
+			y : y,
+			w : 'auto',
+			h : 'auto'
+		}
+	});
+}
+
+	// 定义
+	var myComponent1 = Vue.extend({
+	  	template: '<div id="com1">A custom component1{{x}}：{{name}}+{{code}}</div>',
+  		data : function(){
+  			return {
+	  			name:"组件1",
+	  			code:"iuap Component 1001",
+	  			x:"bbb"
+	  		}
+		}
+	});
+	// 注册
+	Vue.component('my-component1', myComponent1)
+
+
+
+	var my2 = document.createElement('my-component2')
+	document.getElementById("mycomponents").appendChild(my2);
+	// 注册
+	Vue.component('my-component2', Vue.extend({
+	  		template: '<div id="com2">component2:{{name}}-{{code}}</div>',
+	  		data:function(){
+	  			return {
+		  			name:"李四",
+		  			code:"UF1002"
+		  		}
+		  	}
+		})
+	)
+
+
+	var my3 = document.createElement('my-component3')
+	document.getElementById("mycomponents").appendChild(my3);
+	// 注册
+	Vue.component('my-component3', Vue.extend({
+	  		template: '<div id="com3">component3 {{x}}:{{name}}-{{code}}</div>',
+		
+	  		el:function(){return "#mycomponents"}
+		})
+	)
+
+	// 创建根实例
+	new Vue({
+	  	el: '#mycomponents',
+	  	data:{
+	  		name:"ppppp",
+	  		code:"qqqqqqqq",
+	  		x:"kkk"
+
+	  	}
+	})
+
+
+
+
+
+	new Vue({
+	  	el: '#app',
+	  	data: {
+	    	info: 'Hello iuap mobile!',
+	  	  	todos:[{text:"aa"},{text:"bb"},{text:"cc"},{text:"dd"}]
+	  	},
+	  	methods: {
+   	 		add: function () {
+      			this.info +=' new';
+      			this.todos.push({text: (new Date()).toLocaleString()}); 
+    		},
+    		remove: function () {
+      			this.info = this.info.substring(0,this.info.length-4) ;
+      			this.todos.pop(); 
+    		},
+    		removeTodo: function (index) {
+      			this.todos.splice(index, 1)
+    		}
+  		},
+  		created: function () {
+		    // `this` 指向 vm 实例
+		    console.log('created is: ' + this.info)
+		},
+		beforeCompile: function () {
+		    // `this` 指向 vm 实例
+		    console.log('beforeCompile is: ' + this.info)
+		},
+		compiled: function () {
+		    // `this` 指向 vm 实例
+		    console.log('compiled is: ' + this.info)
+		},ready: function () {
+		    // `this` 指向 vm 实例
+		    console.log('ready is: ' + this.info)
+		},beforeDestroy: function () {
+		    // `this` 指向 vm 实例
+		    console.log('beforeDestroy is: ' + this.info)
+		},destroy: function () {
+		    // `this` 指向 vm 实例
+		    console.log('destroy is: ' + this.info)
+		}
+	})
+
+
+    
+
+
+	workbench.createComponent("um-header", {title:"iuap mobile xxfff"});
+
+
+
+	workbench.createComponent("um-footer", {
+		data: [{title:"xxx", iconfont:"ti-comments"},
+			{title:"日程q", iconfont:"ti-notepad"},
+			{title:"通讯录", iconfont:"ti-agenda"},
+			{title:"我", iconfont:"ti-user"}
+		]}
+	);
+	/*
+	workbench.createComponent("um-APPManager", {
+				data : [{label:"审批",icon:'ss.png'},{label:"新闻",icon:'xw.png'}],
+				colum: "4"
+	});
+*/
+
+
+
+
+
+	new Vue({
+		el: 'body',
+		data: {
+			currentView: 'home'
+		},
+	  	components: {
+		    home: {
+	  			template: '<div>A custom component HOME ++ {{name}}!</div>'
+			},
+		    contacts: {
+			  template: '<div>A custom component Contacts ++ {{name}}!</div>'
+			},
+		    settings:{
+			  template: '<div>A custom component Settings ++ {{name}}!</div>'
+			}
+	  	}
+	})
---
+++ js/workbench.js
@@ -0,0 +1,72 @@
+var workbench = {
+
+	init : function(){
+	
+	},
+	createComponent : function(type, settings){
+		if(type == 'um-header'){
+			// 定义
+			var header = Vue.extend({
+			  	template: '<div id="header" class="um-header">'
+						+'<a href="#" class="um-back">返回</a>'
+						+'<h3>{{aa}}</h3>'
+						+'<a href="#" onclick="openwin()" class="um-header-right ti-plus f20"></a>'
+						+'</div>',
+				data: function(){
+					return {
+						aa : settings.title					
+					}
+				}
+			});
+			// 注册
+			Vue.component('um-header', header);
+		}else if(type == "um-footer"){
+		    // 定义
+
+			var footer = Vue.extend({
+			  template: '<div class="um-footer">'
+						+'<div class="um-tabbar-foot">'
+							+'<a id="item0" href="#" class="um-footerbar-item">'
+								+'<i class="' + settings.data[0].iconfont + ' f20"></i>'
+								+'<div class="um-tabbar-item-text">{{first}}</div>'
+							+'</a>'
+							+'<a id="item1" href="#" class="um-footerbar-item"> '
+								+'<i class="' + settings.data[1].iconfont + '  f20"></i>'
+								+'<div class="um-tabbar-item-text">{{second}}</div> '
+							+'</a>'
+							+'<a id="item2" href="#" class="um-footerbar-item active">'
+								+'<i class="' + settings.data[2].iconfont + '  f20"></i>'
+								+'<div class="um-tabbar-item-text">{{third}}</div>'
+							+'</a>'
+							+'<a id="item3" href="#" class="um-footerbar-item">'
+								+'<i class="' + settings.data[3].iconfont + '  f20"></i>'
+								+'<div class="um-tabbar-item-text">{{forth}}</div>'
+							+'</a>'
+						+'</div>'
+					+'</div>',
+				data : function(){
+					return {
+						first: settings.data[0].title,
+						second: settings.data[1].title,
+						third: settings.data[2].title,
+						forth: settings.data[3].title
+					}
+				}
+			});
+			// 注册
+			Vue.component('um-footer', footer);
+		}else if(type == 'um-APPManager'){
+/*
+			var app = new APPManager("#div2", {
+				data : [{label:"审批",icon:'ss.png'},{label:"新闻",icon:'xw.png'}],
+				colum: "4"
+			});
+			app.setIcon();
+			app.setPadding();
+*/
+			
+
+		}
+	}
+	
+}
