6ce035f0dcda41e98e8cb7a28bbdfe984b49bd7b
gct <gct@yonyou.com>
2016-12-09 17:00:21 +0800

    tt

---
+++ js/index - 副本.js
@@ -0,0 +1,449 @@
+﻿   /*底部导航栏与4个内容区域切换*/
+	$(function(){
+		/*
+		$('.um-footerbar>a').click(function(){
+			debugger;
+				$(this).addClass('active').siblings('.active').removeClass('active');
+				var tar=$(this).attr('data-tar');
+				$(tar).addClass('active').siblings('.active').removeClass('active');	
+		});	
+   */
+	});	
+	workbench.createComponent("um-header", {placeholder:"搜订单/商品"});
+	workbench.createComponent("um-header-text", {
+		data: [
+			{title: "消息",bgc:'rgba(246, 246, 246, 1)'}, 
+		]
+    });
+	workbench.createComponent("um-banner", {
+		data: [
+			{content: "./img/g1.jpg"}, 
+			{content: "./img/g2.jpg"}, 
+			{content: "./img/g3.jpg"}
+		]
+    });
+    /*
+    var emmJson = {"deviceid":"D8YDU15A31007455868753026659495","os":"web","userid":"dzk"};
+	var s = $summer.jsonToStr(emmJson);
+	var param = {data: s};
+	$.get("http://172.20.7.98:8080/mobem/app/getapplist",param,function(data){
+		//获取应用列表后的处理
+		var d = ($summer.strToJson(data)).data.apps.remove(0);
+		for (var i =0;i<d.length;i++){
+			d[i].img = d[i].webiconurl;
+			d[i].label = d[i].title;
+			d[i].url = d[i].weburl;
+		}
+		workbench.createComponent("um-APPManager",{
+			el:"#uappmanager",
+			data:d,
+			colum:4
+		});
+	});
+	*/
+
+	workbench.createComponent("um-appmanager",{
+		el:"#uappmanager",
+		data:[{
+			"mark":"xbj",
+			"label" : "询报价",
+			"img" : "img/xbj.png",
+			"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+		},
+		{	
+			"mark":"ztb",
+			"label" : "招投标",
+			"img" : "img/ztb.png",
+			"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+		},
+		{
+			"mark":"cs",
+			"label" : "超市",
+			"img" : "img/cs.png",
+			"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+		},
+		{
+			"mark":"jj",
+			"label" : "在线竞价",
+			"img" : "img/jj.png",
+			"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+		},
+		{
+			"mark":"zr",
+			"label" : "供应商准入",
+			"img" : "img/zr.png",
+			"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+		},
+		{
+			"mark":"sh",
+			"label" : "收获",
+			"img" : "img/sh.png",
+			"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+		},
+		{
+			"mark":"dz",
+			"label" : "对账",
+			"img" : "img/dz.png",
+			"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+		}]
+	});
+	
+	workbench.createComponent("um-footer-custom", {
+		data: [{title:"首页", iconfont:"icon-home",target:'#home' },
+			{title:"消息", iconfont:"icon-msg",target:'#message' },
+			{title:"商品", iconfont:"icon-goods",target:'#goods'},
+			{title:"我的", iconfont:"icon-mine",target:'#mine'}
+		]}
+	);
+	workbench.createComponent("um-layout-text", {
+		data: [{title:"待定标", text:"12","url":'http://www.baidu.com'},
+			{title:"待收货", text:"3","url":'http://www.baidu.com'},
+			{title:"待确认对账", text:"3","url":'http://www.baidu.com'},
+			{title:"待发布", text:"5","url":'http://www.baidu.com'},
+			{title:"待审批", text:"1","url":'http://www.baidu.com'}
+			 
+		]}
+	);
+	//点击更多
+	workbench.createComponent("um-application",{
+		data:[
+			{
+				"title":"应用",
+				"arr":[
+					{
+						"label" : "询报价",
+						"img" : "img/xbj.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "招投标",
+						"img" : "img/ztb.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "超市",
+						"img" : "img/cs.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "在线竞价",
+						"img" : "img/jj.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "供应商准入",
+						"img" : "img/zr.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "收获",
+						"img" : "img/sh.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "对账",
+						"img" : "img/dz.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					}
+				]
+			},
+			{
+				"title":"对账",
+				"arr":[
+					{
+						"label" : "询报价",
+						"img" : "img/xbj.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "招投标",
+						"img" : "img/ztb.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "超市",
+						"img" : "img/cs.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "在线竞价",
+						"img" : "img/jj.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "供应商准入",
+						"img" : "img/zr.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "收获",
+						"img" : "img/sh.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "对账",
+						"img" : "img/dz.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					}
+				]
+			},
+			{
+				"title":"采购需求",
+				"arr":[
+					{
+						"label" : "询报价",
+						"img" : "img/xbj.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "招投标",
+						"img" : "img/ztb.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "超市",
+						"img" : "img/cs.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "在线竞价",
+						"img" : "img/jj.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "供应商准入",
+						"img" : "img/zr.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "收获",
+						"img" : "img/sh.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					},
+					{
+						"label" : "对账",
+						"img" : "img/dz.png",
+						"url" : "http://uapma.yonyou.com:8443/weixin/summerShow_web/index.html"
+					}
+				]
+			},
+		]
+	});
+	workbench.createComponent("um-items",{
+		data:[
+			{
+				"img": "img/goods/7.png",
+				"text":"数码电器",
+				"url":""
+			},
+			{
+				"img": "img/goods/1.png",
+				"text":"办公用品",
+				"url":""
+			},
+			{
+				"img": "img/goods/3.png",
+				"text":"机器设备",
+				"url":""
+			},
+			{
+				"img": "img/goods/4.png",
+				"text":"五金工具",
+				"url":""
+			},
+			{
+				"img": "img/goods/2.png",
+				"text":"电工电气",
+				"url":""
+			},
+			{
+				"img": "img/goods/5.png",
+				"text":"精细化工",
+				"url":""
+			},
+			{
+				"img": "img/goods/6.png",
+				"text":"冶金矿产",
+				"url":""
+			}
+		],
+		more:
+			{
+				"img": "img/goods/8.png",
+				"text":"所有分类",
+				"url":""
+			}
+	});
+	workbench.createComponent("um-classify",{
+		data:[
+			{
+				"img": "img/goods/7.png",
+				"text":"数码电器",
+				"url":""
+			},
+			{
+				"img": "img/goods/1.png",
+				"text":"办公用品",
+				"url":""
+			},
+			{
+				"img": "img/goods/3.png",
+				"text":"机器设备",
+				"url":""
+			},
+			{
+				"img": "img/goods/4.png",
+				"text":"五金工具",
+				"url":""
+			},
+			{
+				"img": "img/goods/2.png",
+				"text":"电工电气",
+				"url":""
+			},
+			{
+				"img": "img/goods/5.png",
+				"text":"精细化工",
+				"url":""
+			},
+			{
+				"img": "img/goods/6.png",
+				"text":"冶金矿产",
+				"url":""
+			},
+			{
+				"img": "img/goods/8.png",
+				"text":"所有分类",
+				"url":""
+			}
+		],
+		title:"推荐商品"			
+	});
+	workbench.createComponent("um-message", {
+		data: [{
+        			"sender" : "询报价",
+        			"img" : "./img/org1.png",
+        			"msgNum" : '3',
+        			"lastMsg" : "收到2份#关于“毕节吧电脑采购询价”",
+					"url":'http://www.baidu.com'
+        			 
+        		},{
+        			"sender" : "订单",
+        			"img" : "./img/org2.png",
+        			"msgNum" : '',
+        			"lastMsg" : "您的订单已经安排发货",
+					"url":'http://www.hao123.com'
+        		 
+        		},{
+        			"sender" : "对账",
+        			"img" : "./img/org3.png",
+        			"msgNum" : '2',
+        			"lastMsg" : "金立集团发来一封对账单",
+					"url":'http://www.sina.com.cn/'
+        		 
+        		},{
+        			"sender" : "公告",
+        			"img" : "./img/org4.png",
+        			"msgNum" : '',
+        			"lastMsg" : "系统维护通知",
+					"url":'http://www.baidu.com'
+        		},{
+        			"sender" : "询报价",
+        			"img" : "./img/org1.png",
+        			"msgNum" : '3',
+        			"lastMsg" : "收到2份#关于“毕节吧电脑采购询价”",
+					"url":'http://www.baidu.com'
+        			 
+        		},{
+        			"sender" : "订单",
+        			"img" : "./img/org2.png",
+        			"msgNum" : '',
+        			"lastMsg" : "您的订单已经安排发货",
+					"url":'http://www.hao123.com'
+        		 
+        		},{
+        			"sender" : "对账",
+        			"img" : "./img/org3.png",
+        			"msgNum" : '2',
+        			"lastMsg" : "金立集团发来一封对账单",
+					"url":'http://www.sina.com.cn/'
+        		 
+        		},{
+        			"sender" : "公告",
+        			"img" : "./img/org4.png",
+        			"msgNum" : '',
+        			"lastMsg" : "系统维护通知",
+					"url":'http://www.baidu.com'
+        		},{
+        			"sender" : "询报价",
+        			"img" : "./img/org1.png",
+        			"msgNum" : '3',
+        			"lastMsg" : "收到2份#关于“毕节吧电脑采购询价”",
+					"url":'http://www.baidu.com'
+        			 
+        		},{
+        			"sender" : "订单",
+        			"img" : "./img/org2.png",
+        			"msgNum" : '',
+        			"lastMsg" : "您的订单已经安排发货",
+					"url":'http://www.hao123.com'
+        		 
+        		},{
+        			"sender" : "对账",
+        			"img" : "./img/org3.png",
+        			"msgNum" : '2',
+        			"lastMsg" : "金立集团发来一封对账单",
+					"url":'http://www.sina.com.cn/'
+        		 
+        		},{
+        			"sender" : "公告",
+        			"img" : "./img/org4.png",
+        			"msgNum" : '',
+        			"lastMsg" : "系统维护通知",
+					"url":'http://www.baidu.com'
+        		}                        
+		]}
+	);
+var rootVue = new Vue({
+  	el: '#body0',
+  	data: {
+    	info: 'Hello iuap mobile!',
+  	  	todos:[{text:"aa"},{text:"bb"},{text:"cc"},{text:"dd"}]
+  	},
+  	methods: {
+ 		add: function () {
+  			this.info +=' new';
+  			this.todos.push({text: (new Date()).toLocaleString()}); 
+		},
+		remove: function () {
+  			this.info = this.info.substring(0,this.info.length-4) ;
+  			this.todos.pop(); 
+		},
+		removeTodo: function (index) {
+  			this.todos.splice(index, 1)
+		}
+	},
+	created: function () {
+	    // `this` 指向 vm 实例
+	   // console.log('created is: ' + this.info)
+	},
+	beforeCompile: function () {
+	    // `this` 指向 vm 实例
+	   // console.log('beforeCompile is: ' + this.info)
+	},
+	compiled: function () {
+	    // `this` 指向 vm 实例
+	    //console.log('compiled is: ' + this.info)
+	},ready: function () {
+	    // `this` 指向 vm 实例
+	   // console.log('ready is: ' + this.info)
+	},beforeDestroy: function () {
+	    // `this` 指向 vm 实例
+	   // console.log('beforeDestroy is: ' + this.info)
+	},destroy: function () {
+	    // `this` 指向 vm 实例
+	   // console.log('destroy is: ' + this.info)
+	}
+})
+ 
+ 
---
+++ js/workbench - 副本.js
@@ -0,0 +1,383 @@
+
+var workbench = {
+
+	init : function(){
+	
+	},
+	createComponent : function(type, settings){
+		if(type == 'um-header'){
+			// 定义
+			var header = Vue.extend({
+				template:'<div class="um-header-fixed">'
+    						+'<a class="fl um-scan">'
+    						+'<img src="img/scan.png" alt="" />'
+    						+'<div class="f12">扫一扫</div>'
+    						+'</a>'
+           					+' <div class="um-input-group">'
+	         				+'<span class="t-search"></span>'
+	         				+'<input type="search" class="form-control" :placeholder="aa">'
+	         				+'<span class="t-camera"></span>'
+           				    +'</div>'
+    						+'</div>',
+				data: function(){
+					return {
+						aa : settings.placeholder					
+					}
+				}
+			});
+			// 注册
+			Vue.component('um-header', header);
+		}else if(type=='um-header-text'){
+			var headertext = Vue.extend({
+			  	template: '<div   class="um-header-fixed" style="background:'+settings.data[0].bgc+'">'
+						+'<p>{{title}}</p>'
+						+'</div>',
+				data: function(){
+					return {
+						title : settings.data[0].title,				
+					}
+				}
+			});
+			// 注册
+			Vue.component('um-header-text', headertext);
+		}else if (type == "um-banner"){
+			var slider = Vue.extend({
+				template : '<div class="um-row">'
+					      +'<div id="iSlider-wrapper" class="iSlider-wrapper"></div>'
+					      +'</div>',
+			    mounted : function(){
+			    	var islider = new iSlider({
+		    			type: 'pic',
+		    			data: settings.data,
+		    			dom: document.getElementById("iSlider-wrapper"),
+		    			isLooping: true,
+		    			animateType: 'default',
+		    			isAutoplay: true,
+		    			animateTime: 800
+		    		});
+		    		islider.addDot();
+			    }
+			});
+			// 注册
+			Vue.component('um-banner', slider);
+			
+		}else if(type == 'um-appmanager'){
+			var APPManager = Vue.extend({
+				template : '<div id="uappmanager"></div>',
+				
+				mounted : function(){
+					//alert(111)
+					//alert(el)
+					$(settings.el).APPManager(settings);
+				}
+			});
+			// 注册
+			Vue.component('um-appmanager', APPManager);
+			//$(settings.el).APPManager(settings);
+			//var v = new APPManager(settings.el,settings);
+		}else if(type == 'um-application'){
+			//$(settings.el).APPManager(settings);
+		}else if(type == "um-footer"){
+		    // 定义
+			var footer = Vue.extend({
+			  template: '<div class="um-footer">'
+						+'<div class="um-tabbar-foot">'
+							+'<a id="item0" href="#" class="um-footerbar-item">'
+								+'<i class="' + settings.data[0].iconfont + ' f20"></i>'
+								+'<div class="um-tabbar-item-text">{{first}}</div>'
+							+'</a>'
+							+'<a id="item1" href="#" class="um-footerbar-item"> '
+								+'<i class="' + settings.data[1].iconfont + '  f20"></i>'
+								+'<div class="um-tabbar-item-text">{{second}}</div> '
+							+'</a>'
+							+'<a id="item2" href="#" class="um-footerbar-item active">'
+								+'<i class="' + settings.data[2].iconfont + '  f20"></i>'
+								+'<div class="um-tabbar-item-text">{{third}}</div>'
+							+'</a>'
+							+'<a id="item3" href="#" class="um-footerbar-item">'
+								+'<i class="' + settings.data[3].iconfont + '  f20"></i>'
+								+'<div class="um-tabbar-item-text">{{forth}}</div>'
+							+'</a>'
+						+'</div>'
+					+'</div>',
+				data : function(){
+					return {
+						first: settings.data[0].title,
+						second: settings.data[1].title,
+						third: settings.data[2].title,
+						forth: settings.data[3].title
+					}
+				}
+			});
+			// 注册
+			Vue.component('um-footer', footer);
+		}else if(type == "um-footer-custom"){
+		    // 定义
+			var footerCustom = Vue.extend({
+			  template1: '<div class="um-footer">'
+                    +'<div class="um-footerbar">'
+                        +'<a href="#" :class="[um_footerbar_item, A1_active]" data-tar="' + settings.data[0].target + '" v-on:click="increment">'
+                            +'<div class="mb5" v-bind:style="styleObject"></div>'
+                            +'<div class="f14 lh1">{{first}}</div>'
+                        +'</a>'
+                        +'<a href="#" :class="[um_footerbar_item, A2_active]" data-tar="' + settings.data[1].target + '" v-on:click="increment">'
+                            +'<div class="' + settings.data[1].iconfont + ' mb5"></div>'
+                            +'<div class="f14 lh1">{{second}}</div>'
+                        +'</a>'
+                        +'<a href="#" :class="[um_footerbar_item, A3_active]" data-tar="' + settings.data[2].target + '" v-on:click="increment">'
+                            +'<div class="' + settings.data[2].iconfont + ' mb5"></div>'
+                            +'<div class="f14 lh1">{{third}}</div>'
+                        +'</a>'
+                        +'<a href="#" :class="[um_footerbar_item, A4_active]" data-tar="' + settings.data[3].target + '" v-on:click="increment">'
+                            +'<div class="' + settings.data[3].iconfont + ' mb5"></div>'
+                            +'<div class="f14 lh1">{{forth}}</div>'
+                        +'</a>'
+                    +'</div>'
+                +'</div>',
+                 template: '<div class="um-footer">'
+                    +'<div class="um-footerbar" >'
+                        +'<a v-for="x in footerbars" href="#" :class="x.item" data-tar="' + settings.data[0].target + '" v-on:click="increment">'
+                            +'<div class="mb5"></div>'
+                            +'<div class="f14 lh1">{{x.title}}</div>'
+                        +'</a>'
+                    +'</div>'
+                +'</div>',
+				data : function(){
+					return {
+						x_item:"um-footerbar-item",
+						footerbars: [{
+							item: 'um-footerbar-item active',
+							title:'消息',
+							styleObject: {
+							    height: '22px',
+							  	backgroundPosition: 'center center',
+							  	backgroundRepeat:'no-repeat', 
+							  	backgroundSize:'22px 22px',
+
+							    backgroundImage: 'url(img/home-25-active.png)',
+							}
+						},{
+							item: 'um-footerbar-item',
+							title:'商品1',
+							styleObject: {
+							    height: '22px',
+							  	backgroundPosition: 'center center',
+							  	backgroundRepeat:'no-repeat', 
+							  	backgroundSize:'22px 22px',
+
+							    backgroundImage: 'url(img/home-25-active.png)',
+							}
+
+						},{
+							item: 'um-footerbar-item',
+							title:'商品2',
+							styleObject: {
+							    height: '22px',
+							  	backgroundPosition: 'center center',
+							  	backgroundRepeat:'no-repeat', 
+							  	backgroundSize:'22px 22px',
+
+							    backgroundImage: 'url(img/home-25-active.png)',
+							}
+
+						},{
+							item: 'um-footerbar-item',
+							title:'商品',
+							styleObject: {
+							    height: '22px',
+							  	backgroundPosition: 'center center',
+							  	backgroundRepeat:'no-repeat', 
+							  	backgroundSize:'22px 22px',
+
+							    backgroundImage: 'url(img/home-25-active.png)',
+							}
+
+						}],
+						first: settings.data[0].title,
+						second: settings.data[1].title,
+						third: settings.data[2].title,
+						forth: settings.data[3].title,
+						
+						um_footerbar_item: "um-footerbar-item",
+						A1_active: "active",
+							A2_active: false,
+							A3_active: false,
+							A4_active: false
+					}
+				},
+				methods:{
+					increment:function(args){
+						debugger;
+						var srcElement = args.currentTarget;
+						var list = args.currentTarget.parentElement.children
+
+						for(var i=0,len=list.length;i<len;i++){
+							list[i].className = list[i].className.replace(/ active/g, '');
+						}
+
+						srcElement.className += " active";
+						this.styleObject.backgroundImage = 'url(img/home-25.png)';
+return;
+						this.A1_active = '';
+						this.A2_active = "active";
+						this.A3_active = '';
+						this.A4_active = '';
+						return;
+						/*
+						$(srcElement).addClass('active').siblings('.active').removeClass('active');
+						var tar=$(srcElement).attr('data-tar');
+						$(tar).addClass('active').siblings('.active').removeClass('active');	
+						this.styleObject.backgroundImage = 'url(img/home-25-active.png)'
+*/
+					}
+				}
+			});
+			// 注册
+			Vue.component('um-footer-custom', footerCustom);
+		}else if(type=='um-applayout'){
+			var AppLayout = Vue.extend({
+				template: '<div class="um-grid">'
+							+'<div class="um-grid-row tc">'
+								 +'<div >'
+									+'<a href="#" class="um-circle um-black">'
+										+'<img src="'+settings.data[0].img+'" width=40 class="um-img-responsive" alt="">'
+										+'<div class="f12 mt5">{{one}}</div>'
+									+'</a>'
+								+'</div>' 
+								+'<div>'
+									+'<a href="#" class="um-circle um-black">'
+										+'<img src="'+settings.data[1].img+'" width=40 class="um-img-responsive" alt="">'
+										+'<div  class="f12 mt5">{{two}}</div>'
+									+'</a>'
+								+'</div>'
+								+'<div>'
+									+'<a href="#" class="um-circle um-black">'
+										+'<img src="'+settings.data[2].img+'" width=40 class="um-img-responsive" alt="">'
+										+'<div  class="f12 mt5">{{three}}</div>'
+									+'</a>'
+								+'</div>'
+								+'<div>'
+									+'<a href="#" class="um-circle um-black">'
+										+'<img src="'+settings.data[3].img+'" width=40 class="um-img-responsive" alt="">'
+										+'<div  class="f12 mt5">{{four}}</div>'
+									+'</a>'
+								+'</div>'
+							+'</div>'							 
+                       +'</div>',
+				data: function(){
+			  		return{
+			    		 one:settings.data[0].title,
+						 two:settings.data[1].title,
+						 three:settings.data[2].title,
+						 four:settings.data[3].title,
+			  		}
+			  	}
+			});
+			// 注册
+			Vue.component('um-applayout',AppLayout);
+		}else if(type=='um-layout-text'){
+			 
+			var AppText = Vue.extend({
+				template: '<div class="ncontent" >'
+							+'<div class="alist fl um-grid" v-for="list in lists">'
+								+'<a :href="list.url">'
+									+'<div class="title tc">{{list.text}}</div>'
+									+'<div class="f12 mt5 tc">{{list.title}}</div>'
+								+'</a>'
+							+'</div>'
+							+'<div class="alist fl um-grid nphoto">'
+								+'<a href="#toDo">'
+									+'<img src="img/add.png" style="width:20px">'
+								+'</a>'
+							+'</div>'
+						 +'</div>',							 
+				data: function(){
+			  		return{
+			    		 lists:settings.data
+			  		}
+			  	}
+			});
+			// 注册
+			Vue.component('um-layout-text',AppText);
+		}else if(type=="um-items"){
+			var Appitems = Vue.extend({
+				template:'<div class="um-items">'
+						 +'<ul class="clearfix">'
+						 +'<li v-for="list in gridlist">'
+	                         +'<a :href="list.url" class="um-circle um-grey">'
+	                         	+'<img v-bind:src="list.img" width=40 class="um-img-responsive" alt="">'
+	                         	+'<div class="f12 mt5">{{list.text}}</div>'
+	                         +'</a>'
+                         +'</li>'
+                         +'<li>'
+	                         +'<a :href="more.url" class="um-circle um-grey">'
+	                         	+'<img v-bind:src="more.img" width=40 class="um-img-responsive" alt="">'
+	                         	+'<div class="f12 mt5">{{more.text}}</div>'
+	                         +'</a>'
+                         +'</li>'
+                         +'</ul>'
+                         +'</div>',
+                data: function(){
+                	return{
+			    		 gridlist:settings.data,
+			    		 more:settings.more
+			  		}
+                }       
+			});
+			// 注册
+			Vue.component('um-items',Appitems);
+		}else if(type=="um-classify"){
+			var Appclassify = Vue.extend({
+				template:'<div class="um-classify">'
+						 +'<div class="t"><span class="f14 um-grey">{{classifytit}}</span></div>'
+						 +'<ul>'
+						 +'<li v-for="list in classifylist">'
+	                         +'<a :href="list.url" class="um-circle um-grey">'
+	                         	+'<img v-bind:src="list.img" width=40 class="um-img-responsive" alt="">'
+	                         	+'<div class="f14 mt5">{{list.text}}</div>'
+	                         +'</a>'
+                         +'</li>'
+                         +'</ul>'
+                         +'</div>',
+                data: function(){
+                	return{
+			    		 classifylist:settings.data,
+			    		 classifytit: settings.title
+			  		}
+                }       
+			});
+			// 注册
+			Vue.component('um-classify',Appclassify);
+		}else if(type=='um-message'){
+			var Appmessage = Vue.extend({
+				template: '<div class="um-listview-wrap" id="listview">'
+                     	+'<ul class="um-list um-no-active"  >'
+                     		+'<li class="um-listview-row" v-for="list in lists">'
+                     			+'<a :href="list.url" class="um-list-item um-swipe-action um-no-icon">'
+                     				+'<div class="um-swipe-btns">'
+                     					+'<span class="um-swipe-btn um-delete">删除</span>'
+                     				+'</div>'
+                     				+'<div class="um-list-item-media msg-info">'
+                     					+'<img  v-bind:src="list.img" width=50>'
+                     					+'<span class="um-badge">{{list.msgNum}}</span>'
+                     				+'</div>'
+                     				+'<div class="um-list-item-inner ml5">'
+                     					+'<div class="um-list-item-body">'
+                     						+'<h4 class="um-media-heading fb f16 um-text-overflow tl" >{{list.sender}}</h4>'
+                     						+'<p class="um-gray f14 um-text-overflow tl">{{list.lastMsg}}</p>'
+                     					+'</div>'
+                     				+'</div>' 
+                     			+'</a>'
+                     		+'</li>'
+                     	+'</ul>'
+                    +'</div>',
+				data: function(){
+			  		return {lists:settings.data}
+			  	}
+			});
+			// 注册
+			Vue.component('um-message',Appmessage);
+		}
+	}
+	
+}
